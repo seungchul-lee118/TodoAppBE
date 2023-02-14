91134d1f254013e7fff8206b6645843b94fa8ba5
seungchul <68181728+seungchul-lee118@users.noreply.github.com>
2023-02-14 16:27:35 +0900

    Add HomeController

---
+++ src/main/java/com/codestates/todoAppBE/TodoAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.todoAppBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppBeApplication.class, args);
+	}
+
+}
